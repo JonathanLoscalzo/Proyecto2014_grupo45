6211d0815911d34ea36cdaab17e487d8d2302b74
JonathanLoscalzo <loscalzo.jony@gmail.com>
2014-09-01 00:35:17 -0300

    Se agregan maquetas semiterminadas de login, index, abmalimentos y listado-alimentos. Se agregan fotos de página principal

---
+++ vistas/BACKEND/menu-bar.php
@@ -0,0 +1,10 @@
+<ul>
+	<li>ABM de donantes</li>
+	<li>ABM de entidades receptoras</li>
+	<li>ABM de alimentos</li>
+	<li>ABM de turnos de entrega</li>
+	<li>ABM de servicios prestados</li>
+	<li>Confección y entrega de pedidos</li>
+	<li>Consulta stock de alimentos</li>
+	<li>Listado de alimentos</li>
+</ul>
