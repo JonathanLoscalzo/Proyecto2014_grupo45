f940a9a85ce59c26306faa712995324d940834e4
JonathanLoscalzo <loscalzo.jony@gmail.com>
2014-09-14 14:46:20 -0300

    Cambios en css backend.

--- vistas/BACKEND/index.php
+++ vistas/BACKEND/index.php
@@ -4,37 +4,29 @@
 	<title>Banco de Alimentos</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<!-- BackEnd page	 -->
-        <link href="/css/backend_index.css" rel="stylesheet">
+	<link href="/css/backend_index.css" rel="stylesheet">
+	<link href="../../css/style-backend.css" rel="stylesheet">
 </head>
 <body>
-	<header>
-	<p>Bienvenido! Admin! - <a href="../HOME/index.html">cerrar sesión</a></p>
-	</header>
-	<nav>
-		<img id ="logo-img" src="../../images/logo-web.jpg"/>
-		<!-- sacar esto a un archivo-->
-		<ul>
-			<li>ABM de donantes</li>
-			<li>ABM de entidades receptoras</li>
-			<li><a href ="./ABMalimentos.html">ABM de alimentos</a></li>
-			<li>ABM de turnos de entrega</li>
-			<li>ABM de servicios prestados</li>
-			<li>Confección y entrega de pedidos</li>
-			<li>Consulta stock de alimentos</li>
-			<li><a href="./listado_alimentos.html">Listado de alimentos</a></li>
-		</ul>
-		
-	</nav>
-	<aside>
-		
-	</aside>
-	<div id="cuerpo-principal">
-		
+	<div id="wrapper">
+		<div id="headerwrap">
+			<?php
+				include '_header-backend.php' 
+			?>
+		</div>
+		<div id="navigationwrap">
+			<?
+				include 'menu-bar.php';
+			?>
+
+		</div>
+
+
+		<?
+			include './../_footer.php'
+		?>
 	</div>
 
-	<footer>
-		© 2013 Banco Alimentario de La Plata Recuperando alimentos de La Plata para ayudar a los que mas necesitan :)	
-	</footer>
 
 </body>
 </html>
--- vistas/BACKEND/menu-bar.php
+++ vistas/BACKEND/menu-bar.php
@@ -1,14 +1,12 @@
 <nav>
-	<img id ="logo-img" src="../../images/logo-web.jpg"/>
-	<!-- sacar esto a un archivo-->
 	<ul>
 		<li>ABM de donantes</li>
 		<li>ABM de entidades receptoras</li>
-		<li><a href ="./ABMalimentos.html">ABM de alimentos</a></li>
+		<li><a href ="./ABMalimentos.php">ABM de alimentos</a></li>
 		<li>ABM de turnos de entrega</li>
 		<li>ABM de servicios prestados</li>
 		<li>Confección y entrega de pedidos</li>
 		<li>Consulta stock de alimentos</li>
-		<li><a href="./listado_alimentos.html">Listado de alimentos</a></li>
+		<li><a href="./listado_alimentos.php">Listado de alimentos</a></li>
 	</ul>
 </nav>
