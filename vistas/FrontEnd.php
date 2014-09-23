8b18705c5a65cfdb12011385b7de7f2836888fd7
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2014-09-23 10:37:32 -0300

    cambios

---
+++ vistas/FrontEnd.php
@@ -0,0 +1,19 @@
+<?php
+require_once('vistas/TwigView.php')
+class FrontEnd extends TwigView {
+	public function index(){
+		echo self::getTwig()->render('frontend/index.php');
+	}
+	public function login(){
+		echo self::getTwig()->render('frontend/login.php');
+	}
+	public function proyectos(){
+		echo self::getTwig()->render('frontend/Proyectos.php');
+	}
+	public function voluntariado(){
+		echo self::getTwig()->render('frontend/Voluntariado.php');
+	}
+	public function dona_ahora(){
+		echo self::getTwig()->render('frontend/Dona-ahora.php');
+	}
+}
---
+++ vistas/TwigView.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * Description of TwigView
+ */
+require_once './vendor/autoload.php';
+
+abstract class TwigView {
+
+    private static $twig;
+
+    public static function getTwig() {
+
+        if (!isset(self::$twig)) {
+
+            Twig_Autoloader::register();
+            $loader = new Twig_Loader_Filesystem('./templates');
+            self::$twig = new Twig_Environment($loader);
+        }
+        return self::$twig;
+    }
+
+}
