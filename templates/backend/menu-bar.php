0b36cf7849cec04e63a3f9451925f44614423d27
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2014-09-23 09:37:17 -0300

    Se agregan templastes sin terminar

---
+++ templates/backend/menu-bar.php
@@ -0,0 +1,12 @@
+<nav>
+	<ul>
+		<li><a href="./ABMdonantes.php">ABM de donantes</a></li>
+		<li><a href="./ABMentidadesReceptoras.php">ABM de entidades receptoras</a></li>
+		<li><a href ="./ABMalimentos.php">ABM de alimentos</a></li>
+		<li>ABM de turnos de entrega</li>
+		<li>ABM de servicios prestados</li>
+		<li>Confección y entrega de pedidos</li>
+		<li>Consulta stock de alimentos</li>
+		<li><a href="./listado_alimentos.php">Listado de alimentos</a></li>
+	</ul>
+</nav>
---
+++ templates/frontend/_aside-content.php
@@ -0,0 +1,20 @@
+<aside>
+	<div class="block">
+		<a href="./Dona-ahora.php">
+		<h3 class="image-title">Doná ahora!</h3>
+		<img alt ="image-aside-1"src="../../images/nene-con-hambre.png"/>
+		</a>
+	</div>
+	<div class="block">
+		<a href="./Voluntariado.php">
+		<h3 class="image-title" >Voluntariado</h3>
+		<img alt="image-aside-2" src="../../images/sumate-como-voluntario.jpg"/>
+		</a>
+	</div>
+	<div class="block">
+		<a href="./Proyectos.php">
+		<h3 class="image-title">Proyectos</h3>
+		<img alt="image-aside-3" src="../../images/imagen-comedores.jpg"/>
+		</a>
+	</div>
+</aside>
