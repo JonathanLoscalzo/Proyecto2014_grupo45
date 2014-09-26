bf9f252efeb025c50d4d561a54bf60496b67872f
Dante Barba <dantebarba@gmail.com>
2014-09-26 18:10:20 -0300

    trabajando en receptora, agregue un comment en donantes respondiendo otro

--- model/EntidadReceptoraRepository.php
+++ model/EntidadReceptoraRepository.php
@@ -12,5 +12,29 @@
  * @author jloscalzo
  */
 class EntidadReceptoraRepository extends PDORepository{
-    
+    private static $instance = null;
+
+    public static function getInstance() {
+
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+    public function add($entidadReceptora) {
+        $sql = "INSERT INTO entidad_receptora VALUES(?,?,?,?,?,?)";
+        $args = $entidadReceptora->getArray();
+        $mapper = "";
+        $answer = $this->queryList($sql, $args, $mapper);
+        return $answer;
+    }
+    public function edit($entidadReceptora) {
+        $sql = "UPDATE `entidad_receptora` 
+                SET `Id`=?,`razon_social`=?,`telefono`=?,
+                `domicilio`=?,`estado_entidad_Id`=?,`necesidad_entidad_Id`=?,
+                `servicio_prestado_Id`=? 
+                WHERE id=?";
+        
+    }
 }
