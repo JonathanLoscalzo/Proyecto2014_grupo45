623c79130b73860efdcdb316ab5ae5b6734405bb
JonathanLoscalzo <loscalzo.jony@gmail.com>
2014-09-27 14:24:25 -0300

    problema de no mostrar

--- templates/backend/_header-backend.php
+++ templates/backend/_header-backend.php
@@ -1,5 +1,5 @@
 
 <header>	
-	<div style="text-align: right" ><p>Bienvenido {{session.username}} - <a href="{{server}}/logout"><img src="images/icons/glyphicons_203_lock.png" alt="candado">Cerrar sesión</a></p></div>
-	<img alt ="logo-empresa" id ="logo-img" style ="border-radius: 15px; "src="images/logo-web.jpg"/>
+	<div style="text-align: right" ><p>Bienvenido {{session.username}} - <a href="{{server}}/logout"><img src="../../images/icons/glyphicons_203_lock.png" alt="candado">Cerrar sesión</a></p></div>
+	<img alt ="logo-empresa" id ="logo-img" style ="border-radius: 15px; "src="../../images/logo-web.jpg"/>
 </header>
