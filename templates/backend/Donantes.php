56e96ba38a362fc46ebeae008001420dddcd2e24
JonathanLoscalzo <loscalzo.jony@gmail.com>
2014-09-28 00:28:30 -0300

    arreglos en vista donante

--- templates/backend/Donantes.php
+++ templates/backend/Donantes.php
@@ -18,46 +18,56 @@
     <div id = "accordion">
         <h3>ALTA</h3>
         <div>
-            <form action="./donantes/add" method="POST" id="alta">
-                <div class="conj-block">
-                    <label for="razonSocial" style = "display : block">Razon Social: </label><input id="razonSocial" name="razonSocial" type="number" >
-                    <label for="nombre" style = "display : block">Nombre: </label><input id="nombre" name="nombre" type="text" >
-                    <label for="apellido" style = "display : block">Apellido: </label><input id="apellido" name="apellido" type="text" >
+            <form action="./{{server}}donantes/add" method="POST" id="alta">
+                  <div class="conj-block">
+                      <label for="razonSocial" style = "display : block">Razon Social: </label>
+                      <input id="razonSocial" name="razonSocial" type="text" maxlength="100" required placeholder="Ingrese Razon Social">
+                      <label for="nombre" style = "display : block">Nombre: </label>
+                      <input id="nombre" name="nombre" type="text" maxlength="50" required placeholder="Ingrese su nombre"> 
+                      <label for="apellido" style = "display : block">Apellido: </label>
+                      <input id="apellido" name="apellido" type="text" maxlength="50" required placeholder="Ingrese su apellido">
                 </div>
                 <div class="conj-block">
-                    <label for="telefono" style = "display : block">Teléfono: </label><input id="telefono" name="telefono" type="number" >
-                    <label for="direccion" style = "display : block">Domicilio: </label><input id="domicilio" name="domicilio" type="text" >
-                    <label for="email" style = "display : block">Correo Electrónico: </label><input id="email" name="email" type="email" >
+                    <label for="telefono" style = "display : block">Teléfono: </label>
+                    <input id="telefono" name="telefono" type="number" maxlength="30" required placeholder="Ej: 4561234">
+                    <label for="email" style = "display : block">Correo Electrónico: </label>
+                    <input id="email" name="email" type="email" maxlength="50" required placeholder="Ej: Mail@email.com">
                 </div>
-                <button type="submit"> Enviar </button>
+                <div class="conj-block">
+                    <label for="direccion" style = "display : block">Domicilio: </label>
+                    <textarea rows="3" colspan=5 id="domicilio" name="domicilio" type="text" maxlength="200" required placeholder="Ej: Benito Juarez 2040, Cancha Lejana, La Trompa (200A)">
+                    </textarea>
+                    <button type="submit" name="submit" style="display:block"> Enviar </button>
+                </div>
             </form>
         </div>
         <h3>Baja y Modificación</h3>
-        <div>
+        <div class="tabla-class">
             <table id="tabla-donantes">
                 <thead>
-                <th>Razón Social</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Teléfono</th>
-                <th>Domicilio</th>
-                <th>Email</th>
-
+                    <tr>
+                        <th>Razón Social</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Teléfono</th>
+                        <th>Domicilio</th>
+                        <th>Email</th>
+                        <th colspan="2"></th>
+                    </tr>
                 </thead>
                 <tbody>
                     {% for donante in donantes %}
-                <td>
-                <tr> {{ donante.razonSocial }} </tr>
-                <tr> {{ donante.nombre }} </tr>
-                <tr> {{ donante.apellido }} </tr>
-                <tr> {{ donante.telefono }} </tr>
-                <tr> {{ donante.domicilio }} </tr>
-                <tr> {{ donante.email }} </tr>
-
-                <tr><a href="./donantes/edit/{{ donante.id }}"><img src="../images/icons/glyphicons_235_pen.png" alt="modificar"></a></tr>
-                <tr><a href="./donantes/remove/{{ donante.id }}"><img src="../images/icons/glyphicons_197_remove.png" alt="borrar"></a></tr>
-                </td>
-                {% endfor %}
+                    <tr>
+                        <td> {{ donante.razonSocial }} </td>
+                        <td> {{ donante.nombre }} </td>
+                        <td> {{ donante.apellido }} </td>
+                        <td> {{ donante.telefono }} </td>
+                        <td> {{ donante.domicilio }} </td>
+                        <td> {{ donante.email }} </td>
+                        <td><a href="{{server}}donantes/edit/{{ donante.id }}"><img src="{{server}}images/icons/glyphicons_235_pen.png" alt="modificar"></a></td>
+                        <td><a href="{{server}}donantes/remove/{{ donante.id }}"><img src="{{server}}images/icons/glyphicons_197_remove.png" alt="borrar"></a></td>
+                    </tr>
+                    {% endfor %}
                 </tbody>
             </table>
         </div>
@@ -68,11 +78,8 @@
 {% block scripts %}
 <script>
 $(document).ready(function () {
-
     $('#accordion').accordion({collapsible: true});
-    $('#tabla-donantes').dataTable({
-        data: datos
-    });
+    $('#tabla-donantes').dataTable();
 });
 
 </script>
