9d7c6b126892f320b5c86168e9be69b92981419c
Dante Barba <dantebarba@gmail.com>
2014-10-20 04:01:57 -0300

    -el css en la tabla no funciona. Verificar.

--- templates/frontend/lista_donantes.php
+++ templates/frontend/lista_donantes.php
@@ -1,12 +1,5 @@
 {% extends "frontend_layout.php" %}
-{% block head %}
-{{ parent() }}
-<link rel="stylesheet" type="text/css" href="{{server}}css/sunny/jquery-ui-1.9.2.custom.css">
-<script type="text/javascript" src="{{server}}js/plugins/jquery-2.1.2.js"></script>
-<script type="text/javascript" src="{{server}}js/plugins/jquery-ui-1.9.2.custom.js"></script>
-<script type="text/javascript" src="{{server}}js/plugins/jquery-ui-1.11.js"></script>
-<script type="text/javascript" src="{{server}}js/plugins/jquery.dataTables-1.10.2.min.js"></script>
-{% endblock %}
+
 {% block content %}
 <div id = "content">
 <!--    Haria falta css aca-->
