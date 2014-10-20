3a1448f304f04adc9ba378b5b3189de157a4e147
Dante Barba <dantebarba@gmail.com>
2014-10-20 03:31:44 -0300

    Armando listar donantes

--- templates/frontend/lista_donantes.php
+++ templates/frontend/lista_donantes.php
@@ -1,36 +1,45 @@
+{% extends "frontend_layout.php" %}
+
+{% block head %}
+<script src="{{server}}js/plugins/jquery-2.1.2.js"></script>
+
+<script type="text/javascript" charset="utf8" src="{{server}}js/plugins/jquery.dataTables-1.10.2.min.js"></script>
+{% endblock %}
 
 {% block content %}
 <div id = "content">
 <!--    Haria falta css aca-->
     <h3> LISTADO DE DONANTES </h3>
     <div class="tabla-class">
-        <table id="tabla-alimentos">
+        <table id="tabla-donantes">
             <thead>
                 <tr>
-                    <th>Tipo</th>
-                    <th>Vencimiento</th>
-                    <th>Contenido</th>
-                    <th>Peso Unitario</th>
-                    <th>Stock</th>
-                    <th>Reservado</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
                 </tr>
             </thead>
 
             <tbody tbody style="text-align: center">
-                {% for detalle in detalles %}
+                {% for donante in donantes %}
 <!--                deben ir todos los alimentos con stock > 0 aca?-->
-                {% if detalle.stock > 0 %}
                 <tr>
-                    <td> {{ detalle.alimento_codigo }} </td>
-                    <td> {{ detalle.fecha_vencimiento }} </td>
-                    <td> {{ detalle.contenido }} </td>
-                    <td> {{ detalle.peso_unitario }} </td>
-                    <td> {{ detalle.stock }} </td>
-                    <td> {{ detalle.reservado }} </td>
+                    <td> {{ donante.nombre }} </td>
+                    <td> {{ donante.apellido  }} </td>
+                </tr>
             </tbody>
-            {% endif %}
             {% endfor %}
         </table>
     </div>
 </div>
+{% endblock %}
+
+{% block scripts %}
+<script>
+$(document).ready(function () {
+    $('#tabla-donantes').dataTable({
+        searching: false,
+        paging: false
+    });
+});
+</script>
 {% endblock %}
