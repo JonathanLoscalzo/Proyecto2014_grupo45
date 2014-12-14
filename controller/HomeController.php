6983119294968605b03eb3e768b6702705ba695a
Dante Barba <dantebarba@gmail.com>
2014-12-14 19:08:01 -0300

    -Probando Linkedin Connect.

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -3,6 +3,8 @@
 include_once("Controller.php");
 include_once("repository/DonanteRepository.php");
 include_once("model/DonanteModel.php");
+include_once("model/OAuthModel.php");
+include_once("repository/ConfiguracionRepository.php");
 
 class HomeController extends Controller {
 
@@ -20,7 +22,25 @@ class HomeController extends Controller {
     protected function __construct() {
         
     }
+    private function getDataLinkedin($credentials) {
+        $oauth = new OAuth($credentials->getApi_key(), $credentials->getApi_secret());
+        $oauth->setToken($credentials->getToken_id(), $credentials->getToken_secret());
 
+        $params = array();
+        $headers = array();
+        $method = OAUTH_HTTP_METHOD_GET;
+
+        // Specify LinkedIn API endpoint to retrieve your own profile
+        $url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,location:(name),summary,email-address,honors-awards)?format=json";
+
+        // By default, the LinkedIn API responses are in XML format. If you prefer JSON, simply specify the format in your call
+        // $url = "https://api.linkedin.com/v1/people/~?format=json";
+
+        // Make call to LinkedIn to retrieve your own profile
+        $oauth->fetch($url, $params, $method, $headers);
+
+        return json_decode($oauth->getLastResponse());
+    }
     public function index() {
         $view = new FrontEndView();
         $view->index();
@@ -60,7 +80,13 @@ class HomeController extends Controller {
     public function acerca_de() {
         // LOGIN LINKEDIN and retrive data
         $view = new FrontEndView();
-        $linkedin_data = "";
+        $credentials = ConfiguracionRepository::getInstance()->getAllOauth();
+        if ($credentials) {
+            $linkedin_data = $this->getDataLinkedin($credentials);
+        }
+        else {
+            $linkedin_data = "No esta configurado el enlace";
+        }
         $view->acerca_de($linkedin_data);
     }
     public function create($entidad) {
