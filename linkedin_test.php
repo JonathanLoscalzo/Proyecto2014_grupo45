f8753aa89e648a035fcf58ccde74a7202216836b
Dante Barba <dantebarba@gmail.com>
2014-12-14 20:01:54 -0300

    viendo mensajes de error

--- linkedin_test.php
+++ linkedin_test.php
@@ -8,7 +8,7 @@
 
 
 
-$oauth = new OAuth("750f8wbrxd35z2", "pmLeubPRbY5brPNd");
+$oauth = new OAuth("750f8wbrxd35zJ", "pmLeubPRbY5brPNd");
 $oauth->setToken("78b0d37d-0c44-4543-bdfb-0febf50b8b94", "b1f894f4-4129-4c07-85e4-563d19776b0f");
  
 $params = array();
