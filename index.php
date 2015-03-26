a6d6f5c457a4d14482d30e64fb4327c61c896916
Dante Barba <dantebarba@gmail.com>
2015-03-26 12:45:27 -0300

    -test, -hay un problema con el PROTOCOLO -Me parece que pasa por ahi el tema de laravel.

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SE
 
 $acciones = explode("/", $_SERVER['REQUEST_URI']);
 
-header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/final/public/');
+header('Location: https://'.$_SERVER['HTTP_HOST'].'/final/public/');
 exit();
 
 if (isset($_POST['date'])) {
